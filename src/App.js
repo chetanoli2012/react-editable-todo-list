63251f1335a7a9ce4cf7b2a985b623d21bab4b65
Chetan Oli <chetanoli@Chetans-MacBook-Pro.local>
2023-10-16 14:03:05 +0545

    Added code for editable todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const inputRef = useRef();
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setTodos([
+        ...todos,
+        { text: e.target.value, completed: false, id: Date.now() },
+      ]);
+
+      inputRef.current.value = "";
+    }
+  };
+
+  const handleCompleted = (id) => {
+    const updatedList = todos.map(function (todo) {
+      return todo.id !== id ? todo : { ...todo, completed: !todo.completed };
+    });
+    setTodos(updatedList);
+  };
+
+  const handleDelete = (id) => {
+    const updatedList = todos.filter(function (todo) {
+      return todo.id !== id;
+    });
+    setTodos(updatedList);
+  };
+
+  const handleUpdate = (id, newText) => {
+    const updatedList = todos.map(function (todo) {
+      return todo.id !== id ? todo : { ...todo, text: newText };
+    });
+
+    setTodos(updatedList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        className="todo-input"
+        type="text"
+        placeholder="Enter your todo here..."
+        onKeyDown={handleKeyDown}
+        ref={inputRef}
+      />
+
+      {todos.map(function (todo) {
+        return (
+          <Item
+            key={todo.id}
+            {...todo}
+            handleCompleted={handleCompleted}
+            handleDelete={handleDelete}
+            handleUpdate={handleUpdate}
+          />
+        );
+      })}
     </div>
   );
 }
 
 export default App;
+
+const Item = ({
+  text,
+  completed,
+  id,
+  handleCompleted,
+  handleDelete,
+  handleUpdate,
+}) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [editedText, setEditedText] = useState(text);
+
+  const handleTextChange = (e) => {
+    setEditedText(e.target.value);
+  };
+
+  const handleBlur = () => {
+    setIsEdit(false);
+    handleUpdate(id, editedText);
+  };
+
+  return (
+    <div className="item">
+      <div className="circle" onClick={() => handleCompleted(id)}>
+        {completed ? <span>&#10003;</span> : ""}
+      </div>
+      <div
+        className={completed ? "strike" : ""}
+        onDoubleClick={() => {
+          !completed && setIsEdit(true);
+        }}
+      >
+        {isEdit ? (
+          <input
+            type="text"
+            className="edit-input"
+            value={editedText}
+            onChange={handleTextChange}
+            onBlur={handleBlur}
+          />
+        ) : (
+          text
+        )}
+      </div>
+      <div className="close" onClick={() => handleDelete(id)}>
+        X
+      </div>
+    </div>
+  );
+};
